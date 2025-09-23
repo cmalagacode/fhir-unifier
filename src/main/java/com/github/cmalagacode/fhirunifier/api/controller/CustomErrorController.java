10b61ae7c31a81b389b9743e17ae1aae728ecdb2
Cristian Malaga <cmalaga720@gmail.com>
2025-09-22 23:45:16 -0400

    Exception handler.

--- src/main/java/com/github/cmalagacode/fhirunifier/api/controller/CustomErrorController.java
+++ src/main/java/com/github/cmalagacode/fhirunifier/api/controller/CustomErrorController.java
@@ -1,6 +1,5 @@
 package com.github.cmalagacode.fhirunifier.api.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +13,9 @@ import java.util.Map;
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public ResponseEntity<Map<String, String>> handleError(HttpServletRequest request) {
+    public ResponseEntity<Map<String, String>> handleError() {
         Map<String, String> error = new HashMap<>();
         error.put("error", "Invalid path or request");
-        error.put("request", request.getRequestURL().toString());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
