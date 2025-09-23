fa8eb4b4334fd0774fdd4aff3a3d9b69208b1316
Cristian Malaga <cmalaga720@gmail.com>
2025-09-22 23:18:59 -0400

    Fix Bug. Exception handler.

--- src/main/java/com/github/cmalagacode/fhirunifier/api/error/GlobalExceptionHandler.java
+++ src/main/java/com/github/cmalagacode/fhirunifier/api/error/GlobalExceptionHandler.java
@@ -1,9 +1,10 @@
-package com.github.cmalagacode.fhirunifier.api.exception;
+package com.github.cmalagacode.fhirunifier.api.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,10 +13,10 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+    public Mono<ResponseEntity<Map<String, String>>> handleException(Exception ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", "Something went wrong");
         error.put("message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error));
     }
 }
