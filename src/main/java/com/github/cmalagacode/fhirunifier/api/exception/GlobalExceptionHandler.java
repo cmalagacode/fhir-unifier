652ae36c45ac76c2f62db493d2f640d4910e6068
Cristian Malaga <cmalaga720@gmail.com>
2025-09-22 23:07:12 -0400

    Add Global Error Handler.

---
+++ src/main/java/com/github/cmalagacode/fhirunifier/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.github.cmalagacode.fhirunifier.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Something went wrong");
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
