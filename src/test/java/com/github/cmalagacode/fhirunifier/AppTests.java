b7549f41020dd7ce9da637ec6269203c488bfd89
Cristian Malaga <cmalaga720@gmail.com>
2025-09-23 22:40:52 -0400

    Add Actions & Test.

--- src/test/java/com/github/cmalagacode/fhirunifier/AppTests.java
+++ src/test/java/com/github/cmalagacode/fhirunifier/AppTests.java
@@ -1,11 +1,13 @@
 package com.github.cmalagacode.fhirunifier;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AppTests {
     @Test
-    void contextLoads() {
+    void test() {
+        Assertions.assertEquals(2, 2);
     }
 }
