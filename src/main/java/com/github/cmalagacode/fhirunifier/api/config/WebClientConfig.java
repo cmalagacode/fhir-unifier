093d960da105b8e8ad3e1b2eb9fbca828947155a
Cristian Malaga <cmalaga720@gmail.com>
2025-10-14 16:30:42 -0400

    Fix oauth2 client bug.

--- src/main/java/com/github/cmalagacode/fhirunifier/api/config/WebClientConfig.java
+++ src/main/java/com/github/cmalagacode/fhirunifier/api/config/WebClientConfig.java
@@ -9,12 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.HttpProtocol;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
@@ -31,18 +30,17 @@ public class WebClientConfig {
 
     @Bean
     OAuth2AuthorizedClientManager authorizedClientManager(
-            ClientRegistrationRepository clientRegistrationRepository,
-            OAuth2AuthorizedClientRepository authorizedClientRepository
+            ClientRegistrationRepository clientRegistrationRepository
     ) {
-
         OAuth2AuthorizedClientProvider provider =
                 OAuth2AuthorizedClientProviderBuilder.builder()
                         .clientCredentials()
                         .build();
 
-        DefaultOAuth2AuthorizedClientManager manager =
-                new DefaultOAuth2AuthorizedClientManager(
-                        clientRegistrationRepository, authorizedClientRepository);
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository,
+                        new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository));
         manager.setAuthorizedClientProvider(provider);
 
         return manager;
@@ -82,7 +80,6 @@ public class WebClientConfig {
 
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
                 new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2.setDefaultOAuth2AuthorizedClient(true);
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
