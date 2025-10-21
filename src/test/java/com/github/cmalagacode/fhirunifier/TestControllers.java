50d70c823482f864eb66215923939a8c0295b64e
Cristian Malaga <cmalaga720@gmail.com>
2025-10-21 19:51:49 -0400

    Add CI/CD unit test feature for oauth2. Remove unnecessary mvc config in application.yaml since spring web flux is used. Improved test coverage to include all supported targets.

--- src/test/java/com/github/cmalagacode/fhirunifier/TestControllers.java
+++ src/test/java/com/github/cmalagacode/fhirunifier/TestControllers.java
@@ -1,61 +1,95 @@
 package com.github.cmalagacode.fhirunifier;
 
 import com.github.cmalagacode.fhirunifier.api.model.type.HealthPlanOrganizationName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class TestControllers {
+    @LocalServerPort
+    private int port;
 
-    @Autowired
     private WebTestClient webTestClient;
 
     private final List<String> npiList = Arrays.asList("1467652149", "1477502557");
 
+    @BeforeEach
+    void setup() {
+        this.webTestClient = WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .responseTimeout(java.time.Duration.ofSeconds(300))
+                .build();
+    }
+
     @Test
     public void getCigna() {
-        for (String npi : npiList) {
-            var query = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npi, HealthPlanOrganizationName.CIGNA);
-            webTestClient.get().uri(query)
-                    .exchange()
-                    .expectStatus().isOk();
-        }
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.getFirst(), HealthPlanOrganizationName.CIGNA);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
     public void getUnitedHealthCare() {
-        for (String npi : npiList) {
-            var query = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npi, HealthPlanOrganizationName.UNITED_HEALTH_CARE);
-            webTestClient.get().uri(query)
-                    .exchange()
-                    .expectStatus().isOk();
-        }
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.get(1), HealthPlanOrganizationName.UNITED_HEALTH_CARE);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
     public void getKaiserPermanente() {
-        for (String npi : npiList) {
-            var query = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npi, HealthPlanOrganizationName.KAISER_PERMANENTE);
-            webTestClient.get().uri(query)
-                    .exchange()
-                    .expectStatus().isOk();
-        }
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.getFirst(), HealthPlanOrganizationName.KAISER_PERMANENTE);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
     public void getMolinaHealthCare() {
-        for (String npi : npiList) {
-            var query = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npi, HealthPlanOrganizationName.MOLINA_HEALTHCARE);
-            webTestClient.get().uri(query)
-                    .exchange()
-                    .expectStatus().isOk();
-        }
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.get(1), HealthPlanOrganizationName.MOLINA_HEALTHCARE);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void getHealthCareServiceCorporation() {
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.get(1), HealthPlanOrganizationName.HEALTH_CARE_SERVICE_CORPORATION);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void getIndependenceBlueCross() {
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.getFirst(), HealthPlanOrganizationName.INDEPENDENCE_BLUE_CROSS);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void getElevanceHealth() {
+        var test = String.format("/unifier/v1?npi=%s&model=CONCISE&target=%s", npiList.get(1), HealthPlanOrganizationName.ELEVANCE_HEALTH);
+
+        webTestClient.get().uri(test)
+                .exchange()
+                .expectStatus().isOk();
     }
 }
